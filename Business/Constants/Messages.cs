37a750e3f6541fae8e170a16d34c6f767902223d
Furkan Bakal <f.bakal1997@gmail.com>
2022-01-09 22:57:54 +0300

    update

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string productCountOfCategoryError = "Kategorideki limit sayısını aştınız";
+        public static string productNameExistError = "Bu isimde bir ürün zaten var!";
+        public static string CheckIfCategoryLimitExceded = "Kategori limiti aşıldığı için yeni ürün eklenemiyor";
     }
 }
